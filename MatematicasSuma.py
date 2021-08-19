60d539cb9fb184a09052cab0af189f7a069d590d
aleQP <alejandro_pablo13@hotmail.com>
2021-08-19 12:20:21 -0500

    Propuestas

--- MatematicasSuma.py
+++ MatematicasSuma.py
@@ -1,6 +1,59 @@
 from ModuloMatematicaBasicas import *
+import PySimpleGUI as sg
+
+def opciones(evento):
+	if evento == 'Suma': suma()
+	elif evento == 'Area Rectangulo': areaRectangulo()
+	elif evento == 'Multiplicacion': multiplicacion()
+	else: return "NA"
 
 
-a = int(input("Ingresa el primer #: \n"))
-b = int(input("Ingresa el segundo #: \n"))
-print("Resultado: {}".format(suma(a, b)))
+#Menu 1
+materias = [
+				[sg.Button('Matematicas'), sg.Button('Español'), sg.Button('Fisica'), sg.Button('Algebra'), sg.Button('Calculo'), sg.Button('Programacion')],
+				[sg.HSeparator()],
+				[sg.Button('Suma'), sg.Button('Comparacion Numeros'), sg.Button('Area Rectangulo'), sg.Button('Multiplicacion'),
+				 sg.Button('Division'), sg.Button('Porcentaje'), sg.Button('Teorema de pitagoras'), sg.Button('Area Trapecio'), sg.Button('Factorizacion')]
+			]
+window = sg.Window('SmartVee', materias)
+
+while True: 
+	event, values = window.read()
+	if event == sg.WINDOW_CLOSED or event == 'Cerrar':
+		break
+
+	opciones(event)
+
+	# if event == 'Suma':
+	# 	suma()
+
+window.close()
+
+
+
+#Menu 2
+
+# tab1 = [
+# 		[sg.T('Primer numero')],[sg.I()],
+# 		[sg.T('Segundo numero')],[sg.I()],
+# 		[sg.Button('=')],
+# 		[sg.T('El resultado es: ')]
+# 		]
+# tab2 = [
+# 		[sg.T('Base:')],[sg.I()],
+# 		[sg.T('Altura')],[sg.I()],
+# 		[sg.Button('=')],
+# 		[sg.T('El resultado es: ')]
+# 		]
+# layout = [
+# 			[sg.Button('Matematicas'), sg.Button('Español'), sg.Button('Fisica'), sg.Button('Algebra'), sg.Button('Calculo'), sg.Button('Programacion')],
+# 			[sg.TabGroup([[sg.Tab('Suma', tab1), sg.Tab('Area', tab2)]])],
+# 			[sg.Button('X')]
+# 		]
+
+# window = sg.Window('SmartVee', layout)
+# while True:
+# 	event, values = window.read()
+# 	if event == sg.WINDOW_CLOSED or event == 'X':
+# 		break
+# window.close()
--- ModuloMatematicaBasicas.py
+++ ModuloMatematicaBasicas.py
@@ -1,3 +1,76 @@
-def suma(a, b):
+import PySimpleGUI as sg
+
+
+def suma():
+	layout = [
+			[sg.Text("Primer numero: ")],
+			[sg.Input(key='-INPUT1-')],
+			[sg.Text("Segundo numero: ")],
+			[sg.Input(key='-INPUT2-')],	
+			[sg.T(key='Resultado')],
+			[sg.Button('Ok'), sg.Button('Salir')]
+		]
+
+	window = sg.Window('Suma', layout, modal=True)
+
+	while True:
+		event, values = window.read()
+		if event == sg.WINDOW_CLOSED or event == 'Salir':
+			break
+
+		a=int(values['-INPUT1-'])
+		b=int(values['-INPUT2-'])
+
+		window['Resultado'].update("La suma de {0} + {1} es {2}".format(values['-INPUT1-'], values['-INPUT2-'], sumar(a, b)))
+	window.close()
+
+def sumar(a, b):
 	return (a + b)
-	
+
+
+
+def areaRectangulo():
+	layout = [
+			[sg.T("Base del rectangulo: ")], [sg.I(key='Base')],
+			[sg.T("Altura del rectangulo: ")], [sg.I(key='Altura')],
+			[sg.T(key='Resultado')],
+			[sg.B('OK'), sg.B('X')]
+		]
+	window = sg.Window('Area Rectangulo', layout, modal=True)
+	while True:
+		event, values = window.read()
+		if event == sg.WINDOW_CLOSED or event == 'X':
+			break
+
+		a = int(values['Altura'])
+		b = int(values['Base'])
+		area = (b * a)
+		window['Resultado'].update('El area del rectangulo es: {}'.format(area))
+		# print("Area es igual a: {}".format(area))
+	window.close()
+
+
+def multiplicacion():
+	layout = [
+				[sg.T("Multiplicacion de numeros: ")],[sg.Input(key="Numeros")],
+				[sg.B('OK'), sg.B(' X ')]
+			]
+	window = sg.Window("Multiplicacion", layout, modal=True)
+	while True:
+		event, values = window.read()
+		if event == sg.WINDOW_CLOSED or event == ' X ':
+			break
+		Sep = values['Numeros'].split('*')
+		clean = [num.strip(' ') for num in Sep]
+		Total = [int(i) for i in clean]
+		Fin = 1
+		for x in Total:
+			Fin *= x
+		print(Fin)
+	window.close()
+
+# def mul(n):
+# 	total = 1
+# 	for i in range(0, len(n)):
+# 		total *= n[i]
+# 	print total
